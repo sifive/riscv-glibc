004a8c0ec2d8cd13486fb8d4eb3aaf97b3428222
Jesse Huang <jesse.huang@sifive.com>
2024-02-24 11:49:01 -0800

    riscv/cfi: Adjust setjmp/longjmp for shadow stack to work
    Since longjmp to a previous setjmp'ed state could change the stack
    frame and involves stack frame unwinding, shadow stacks is also required
    to be unwinded.
    
    The unwinding is implemented according to the zicfiss spec by increasing
    the ssp by a page size (4K) at most, to prevent from accidentally point
    to another legal shadow stack page after the adjustment.

--- sysdeps/riscv/bits/setjmp.h
+++ sysdeps/riscv/bits/setjmp.h
@@ -34,6 +34,10 @@ typedef struct __jmp_buf_internal_tag
 #elif !defined __riscv_float_abi_soft
 # error unsupported FLEN
 #endif
+#ifdef __riscv_shadow_stack
+    /* Shadow stack pointer.  */
+    long int __ssp;
+#endif
   } __jmp_buf[1];
 
 #endif /* _RISCV_BITS_SETJMP_H */
