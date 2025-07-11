2b8ed4dc49b02bc98eb9d00a413ab989c6f0aee2
Jesse Huang <jesse.huang@sifive.com>
2025-07-11 06:07:58 -0700

    riscv/cfi: Add __allocate_shadow_stack for mapping new shadow stack
    Co-authored-by: Valentin Haudiquet <valentin.haudiquet@canonical.com>
    Co-authored-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

---
+++ sysdeps/unix/sysv/linux/riscv/bits/mman.h
@@ -0,0 +1,30 @@
+/* Definitions for POSIX memory map interface.  Linux/risc-v version.
+   Copyright (C) 1997-2026 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _SYS_MMAN_H
+# error "Never use <bits/mman.h> directly; include <sys/mman.h> instead."
+#endif
+
+#if defined __USE_MISC && defined __riscv_shadow_stack
+# define SHADOW_STACK_SET_TOKEN 0x1
+#endif
+
+#include <bits/mman-map-flags-generic.h>
+
+/* Include generic Linux declarations.  */
+#include <bits/mman-linux.h>
